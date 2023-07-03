acca3635cd42474fc8cf8c0f84c6a471db6c785e
onaicul1 <lu.larrea@duocuc.cl>
2023-07-03 21:10:10 +0000

    Add Examen.py file to Examen-Transversal-Programacion-y-Redes-Virtualizadas-DRY7122

---
+++ Examen.py
@@ -0,0 +1,8 @@
+Examen DRY7122
+
+integrantes_grupo = ["Jheremi Arias", "Luciano Larrea"]
+
+for integrante in integrantes_grupo:
+    print(integrante)
+
+Este texto se agregó originalmente en la rama de características
